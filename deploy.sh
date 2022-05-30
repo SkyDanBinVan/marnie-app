5e3b58dd6c69a71732fb96c8e5aa88a7ed8df2f0
SkyDanBinVan <daniel.hope@sky.uk>
2022-05-30 16:02:35 +0100

    removed build from .gitignore

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 echo "pulling"
 git pull
 
-echo "building marnie-web-app"
-npm --prefix ./marnie-web run build
+# echo "building marnie-web-app"
+# npm --prefix ./marnie-web run build
 
 echo "building caddy app"
 docker-compose up -d 
